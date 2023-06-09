327f76e0d8a010f1a0b2c8a9a7275406c96797fd
kosarevskiydp <kosarevskiydp@pik.ru>
2023-06-09 23:12:09 +0300

    stake upd

--- src/stake/stake.py
+++ src/stake/stake.py
@@ -3,18 +3,18 @@ import requests
 
 
 def main(username, url):
-    with st.form("get_pool"):
+    with st.form("stake"):
         st.subheader("Stake")
         amount = st.number_input("Stake amount:", min_value=1, value=100, step=1)
         pwd = st.input("Password:", type="password")
-        query_params = {"amount": amount, "password": pwd}
-        response = requests.get(f"{url}/stake/{username}", params=query_params).json()
-        if response["success"]:
-            st.json(response)
-        else:
-            st.error(response)
 
-        st.form_submit_button("Stake")
+        if st.form_submit_button("Stake"):
+            query_params = {"amount": amount, "password": pwd}
+            response = requests.get(f"{url}/stake/{username}", params=query_params).json()
+            if response["success"]:
+                st.json(response)
+            else:
+                st.error(response)
 
 
 if __name__ == "__main__":
