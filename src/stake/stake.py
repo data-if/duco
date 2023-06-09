e1cc543589d3f12e4e6fbbc44344956b3867637b
kosarevskiydp <kosarevskiydp@pik.ru>
2023-06-09 23:19:52 +0300

    stake fix

--- src/stake/stake.py
+++ src/stake/stake.py
@@ -6,7 +6,7 @@ def main(username, url):
     with st.form("stake"):
         st.subheader("Stake")
         amount = st.number_input("Stake amount:", min_value=1, value=100, step=1)
-        pwd = st.input("Password:", type="password")
+        pwd = st.text_input("Password:", type="password")
 
         if st.form_submit_button("Stake"):
             query_params = {"amount": amount, "password": pwd}
