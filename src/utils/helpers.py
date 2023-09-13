9f1d5142b1e6e4e3f31aaeaa08df00b04dbbe9bc
kosarevskiydp <kosarevskiydp@pik.ru>
2023-09-13 21:30:12 +0300

    try to fix

--- src/utils/helpers.py
+++ src/utils/helpers.py
@@ -48,6 +48,7 @@ def calc_by_period(username, url, period, periods, duco_price):
 def get_duco_price(url, username):
     response = requests.get(f"{url}/v3/users/{username}").json()
     if response["success"]:
+        st.code(response["result"])
         return response["result"]["prices"]["max"]
 
 
